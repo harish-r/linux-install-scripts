18968e53397e84eabd4d1358695ef0d35b2d6e70
harish-r <ferrarixx599@gmail.com>
2016-07-09 21:20:02 +0530

    mac_theme.sh for 16.04

---
+++ macubuntu_theme.sh
@@ -0,0 +1,18 @@
+# Install macubuntu icons and themes
+sudo add-apt-repository -y ppa:noobslab/macbuntu
+sudo apt-get update
+sudo apt-get install -y macbuntu-os-icons-lts-v7
+sudo apt-get install -y macbuntu-os-ithemes-lts-v7
+# install plank dock
+sudo apt-get install plank
+# install mac theme for plank
+sudo add-apt-repository -y ppa:noobslab/macbuntu
+sudo apt-get update
+sudo apt-get install -y macbuntu-os-plank-theme-lts-v7
+# Install tweak tools
+sudo apt-get install -y unity-tweak-tool
+sudo apt-get install -y gnome-tweak-tool
+# Install sling cold - spotlight alternative
+sudo add-apt-repository -y ppa:noobslab/macbuntu
+sudo apt-get update
+sudo apt-get install -y slingscold
